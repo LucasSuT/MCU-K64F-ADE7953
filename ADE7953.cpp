8d0c64cd7310e5646ea1e1c988820008e0963c78
Lucassu <l1438s1438@gmail.com>
2020-02-26 10:50:10 +0800

    add second chip control

--- ADE7953.cpp
+++ ADE7953.cpp
@@ -1,10 +1,97 @@
 #include "ADE7953.h"
 
-UARTSerial uart(PA_9, PA_10, 4800);
+// UARTSerial uart(PA_9, PA_10, 4800);
+// UARTSerial *uart = new UARTSerial(PE_1, PE_0, 4800);
 
 bool ADE7953::init()
 {
-    return (read(0x203) == 0xE419) ? SUCCESS : FAIL;
+    if(read(0x102) != 0x8004)return Fail; //check UART communication
+    // verification();
+    write(0x102,0x04); //lock the communication
+    verification();
+    // write(0x040, 0x07); //internal registers write protection
+    // verification();
+    read(0x800);
+    verification();
+    // write(0x800,0x01);
+    // verification();
+    return Success;
+}
+
+int ADE7953::getInstVoltage() //Instantaneous voltage (voltage channel)
+{
+    return (read(0x218));
+}
+
+int ADE7953::getVrms() //VRMS register
+{
+    return (read(0x21C));
+}
+
+//*****************************Channel A*****************************
+
+int ADE7953::getInstCurrentA() //Instantaneous current (Current Channel A)
+{
+    return (read(0x216));
+}
+
+int ADE7953::getIrmsA() //IRMS register (Current Channel A)
+{
+    return (read(0x21A));
+}
+
+int ADE7953::getActiveEnergyA() //Active energy (Current Channel A)
+{
+    return (read(0x21E));
+}
+
+int ADE7953::getReactiveEnergyA() //Reactive energy (Current Channel A)
+{
+    return (read(0x220));
+}
+
+int ADE7953::getApparentEnergyA() //Apparent energy (Current Channel A)
+{
+    return (read(0x222));
+}
+
+//*****************************Channel B*****************************
+
+int ADE7953::getInstCurrentB() //Instantaneous current (Current Channel B)
+{
+    return (read(0x217));
+}
+
+int ADE7953::getIrmsB() //IRMS register (Current Channel B)
+{
+    return (read(0x21B));
+}
+
+int ADE7953::getActiveEnergyB() //Active energy (Current Channel B)
+{
+    return (read(0x21F));
+}
+
+int ADE7953::getReactiveEnergyB() //Reactive energy (Current Channel B)
+{
+    return (read(0x221));
+}
+
+int ADE7953::getApparentEnergyB() //Apparent energy (Current Channel B)
+{
+    return (read(0x223));
+}
+
+//*****************************Test function*****************************
+
+int* ADE7953::verification()
+{
+    int *a = new int[3];
+    a[0] = read(0x0FD); //LAST_OP
+    a[1] = read(0x1FE); //LAST_ADD
+    a[2] = read(0x3FF); //LAST_RWDATA
+    printf("OP= 0x%x ADDR= 0x%x DATA= 0x%x\n", *a, *(a + 1), *(a + 2));
+    return a;
 }
 
 bool ADE7953::write(uint16_t Reg, uint32_t val)
@@ -14,15 +101,15 @@ bool ADE7953::write(uint16_t Reg, uint32_t val)
     int registerSize = getRegisterSize_Byte(Reg);
     if (registerSize < 5)
     {
-        if (uart.writable())
+        if (uart->writable())
         {
-            uart.write(txb, registerSize+3);
+            uart->write(txb, registerSize+3);
             wait_ms(100);
-            return SUCCESS;
+            return Success;
         }
-        else printf("Write - cant write!\n");
+        // else printf("Write - cant write!\n");
     }
-    return FAIL;
+    return Fail;
 }
 
 REG_VALUE ADE7953::read(uint16_t Reg)
@@ -36,20 +123,20 @@ REG_VALUE ADE7953::read(uint16_t Reg)
     { 
         while (1)
         {
-            if (uart.writable() && !lock)
+            if (uart->writable() && !lock)
             {
-                    uart.write(txb, sizeof(txb));
+                    uart->write(txb, sizeof(txb));
                     lock=1;
             }
-            else printf("Read - cant write!\n");
-            if (uart.readable())
+            // else printf("Read - cant write!\n");
+            if (uart->readable())
             {
-                uart.read(rxb, registerSize);
+                uart->read(rxb, registerSize);
                 lock=0;
                 return *regValue;
             }
-            else printf("Read - cant read!\n");
-            wait_ms(100);
+            // else printf("Read - cant read!\n");
+            wait_ms(500);
         }
     }
     return 0;
--- ADE7953.h
+++ ADE7953.h
@@ -2,22 +2,43 @@
 #include <stdint.h>
 
 typedef int REG_VALUE;
-enum Initial
+typedef enum Status
 {
-    FAIL = 0,
-    SUCCESS = 1,
-};
+    Fail = 0,
+    Success = 1,
+} Status;
 
 class ADE7953
 {
 public:
     bool init();
+    int getInstVoltage();
+    int getVrms();
+    int getInstCurrentA();
+    int getIrmsA();
+    int getActiveEnergyA();
+    int getReactiveEnergyA();
+    int getApparentEnergyA();
+    int getInstCurrentB();
+    int getIrmsB();
+    int getActiveEnergyB();
+    int getReactiveEnergyB();
+    int getApparentEnergyB();
+    ADE7953()
+    {
+        uart=new UARTSerial(PA_9, PA_10, 4800);
+    }
+    ADE7953(PinName tx,PinName rx)
+    {
+        uart = new UARTSerial(tx, rx, 4800);
+    }
+    int* verification();
+
+private :
+    int getRegisterSize_Byte(uint16_t);
     bool write(uint16_t, uint32_t);
     REG_VALUE read(uint16_t);
-    int getRegisterSize_Byte(uint16_t);
-    
-
-private : 
     const uint8_t ADE_READ = 0x35;
     const uint8_t ADE_WRITE = 0xCA;
+    UARTSerial *uart=nullptr;
 };
